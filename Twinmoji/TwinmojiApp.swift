ab27bbc515dd06b0246a406d40de346f52349c7f
Sharan Thakur <sharandt19@live.com>
2024-12-24 17:10:04 +0100

    Initial Commit

---
+++ Twinmoji/TwinmojiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwinmojiApp.swift
+//  Twinmoji
+//
+//  Created by Sharan Thakur on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct TwinmojiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
